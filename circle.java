7fc19deb83db543836dadea3dc9deaf7730d8eba
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-16 17:32:32 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ class circle
+ {
+   public static void main(String[]args)
+   {
+     int x=3;
+     float y;
+	y=3.14f*x*x;
+System.out.print("the area of circle is"+y)	;
+}
+}
