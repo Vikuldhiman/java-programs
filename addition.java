3e72be158afc3baf59f59a57c7d22df8b622d8df
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-16 17:32:12 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ class addition
+ {
+   public static void main(String[]args)
+    {
+	   int a,b;
+	    Scanner sum = new Scanner (System.in);
+		System.out.print("enter the first no.");
+		a=sum.nextInt();
+		System.out.print("entr the second no.");
+		b=sum.nextInt();
+	int c=a+b;
+	System.out.println("the sum="+c);
+  }
+}
