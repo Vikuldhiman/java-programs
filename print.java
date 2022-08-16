335ced6ae2db876e5ca1c9d3806b7ede21214a6f
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-16 17:33:24 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Test
+{
+  public static void main (String[]args)
+   { 
+     int x=10;
+	 float y=5.2f;
+	 char z='A';
+	System.out.print(x); 
+	System.out.print(y);
+	System.out.print(z);
+   }
+ }
+	
