d45fea774201d0fb9f23677bf8558e4d97d55550
Vikul Dhiman <107919500+Vikuldhiman@users.noreply.github.com>
2022-08-16 17:33:47 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ class sum
+  {
+    public static void main(String[]args)
+	 {
+	   int x=14;
+	   int y=19;
+	  int z=x+y;
+    System.out.print("the sum of x and y ="+z);
+}
+}	
+	   
